6724aeb584a1d7664be548b3c19066513da54f72
Monique <monique.meas@gmail.com>
2022-08-28 16:36:04 -0400

    add functions to display Departments, Roles and Employees

--- db/schema.sql
+++ db/schema.sql
@@ -3,15 +3,15 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employee;
 CREATE TABLE departments (
     department_name VARCHAR (30),
-    department_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE roles (
-    role_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     job_title VARCHAR (30),
     department_id INTEGER (10),
-    salary DECIMAL (10,2) NOT NULL
-    REFERENCES departments(id) ON DELETE SET NULL
+    salary DECIMAL (10,2) NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employee (
@@ -19,7 +19,9 @@ CREATE TABLE employee (
     first_name VARCHAR (30),
     last_name VARCHAR (30),
     role_id INTEGER (10),
-    manager_id INTEGER(10)
+    manager_id INTEGER(10),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+    
     
 );
     
--- server.js
+++ server.js
@@ -5,19 +5,79 @@ const mysql = require('mysql2');
 //Connect to database
 
 const db = mysql.createConnection(
-    {
-        host:'localhost',
-        port:'3001',
-        user:'root',
-        password: '7141',
-        database: 'employee_db'
+   {
+    host:'localhost',
+      user:'root',
+       password: '7141',
+    database: 'employee_db'
     }
     
 );
 db.connect(function(err){
-    if(err) throw err;
+ if(err) throw err;
    
 
     console.log("Connected to employee_db database.");
+    viewPrompt();
 })
+function viewPrompt () {
+  
+    return inquirer.prompt([
+      {
+        type: 'list',
+        name: 'main',
+        message: 'What would you like to do?',
+        choices:[ 'View all employees', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+      },
+
+    ])
+    .then (function(res){
+        switch(res.main){
+            case 'View all employees':
+                viewAllEm();
+                break;
+            case 'View All Roles':
+              viewRole();
+              break;
+            case 'View All Departments':
+              viewDep();
+        }
+    })
+  };
+
+  function viewAllEm () {
+    const sql = `SELECT * FROM employee`;
+    db.query(sql, (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        console.table(result);
+        viewPrompt();
+    })
+  };
+  function viewRole() {
+    const sql = `SELECT * FROM roles`;
+
+    db.query(sql, (err, result) => {
+      if(err) {
+        console.log(err);
+      }
+      console.table(result);
+      viewPrompt();
+    })
+  };
+  function viewDep () {
+    const sql = `SELECT * FROM departments`;
+
+    db.query(sql, (err, result) => {
+      if(err) {
+        console.log(err);
+      }
+      console.table(result);
+      viewPrompt();
+    })
+  }
+
+
+
 
